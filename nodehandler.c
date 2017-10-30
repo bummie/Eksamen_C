8a4aba69294eec12000fedbe37043fac14aad7c8
Sebastian <seebzei@gmail.com>
2017-10-30 14:51:49 +0100

    Added headerfile

--- nodehandler.c
+++ nodehandler.c
@@ -1,68 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define NODE_NAME_BUFFER_SIZE 256
-#define NODE_CHILD_SIZE_INCREMENTER 5
-
-// Node types
-#define TYPE_NUMERIC 0
-#define TYPE_STRING 1
-#define TYPE_FOLDER 2
-
-// Node structure
-typedef unsigned long ULONG;
-typedef struct _NODE 
-{
-	char* pszName; 
-	ULONG ulIntVal; 
-	char* pszString;
-	int iNodes; 
-	int iArraySizeChildNodes;
-	struct _NODE** pnNodes;
-	
-	//Function pointers
-	struct _NODE* (*GetChildWithKey)(struct _NODE* self, char* key);
-	void (*IncreaseChildArray)(struct _NODE* self);
-	void (*SetInt)(struct _NODE* self, ULONG ulValue);
-	void (*SetString)(struct _NODE* self, char* pszText);
-	ULONG (*GetInt)(struct _NODE* self);
-	char* (*GetString)(struct _NODE* self);
-	int (*GetType)(struct _NODE* self);
-	void (*Print)(struct _NODE* self);
-	void* (*GetValue)(struct _NODE* self);
-	void (*SetValue)(struct _NODE* self, int iValueType, void* nodeValue);
-	
-} NODE;
-
-// Constants
-const char* FILEPATH = "../Data/data.txt";
-
-// Headers
-FILE* loadFile(char* filePath);
-NODE* loadNodesFromFile(char* filePath);
-int findNodeCountInString(char* pszNodeData);
-int parseNodeData(char* pszNodeData);
-char* findNodeNames(char* pszNodeData, int iNodeCount);
-char* findNodeValue(char* pszNodeData);
-NODE* newNode(char* pszName);
-NODE* findNodeByKey(char* nodeKey);
-void Enumerate(char* nodeKey, void (*Callback)(char* nodeName, void* nodeValue));
-void callbackPrint(char* nodeName, void* nodeValue);
-void Delete(char* nodeKey);
-void DeleteByNode(NODE* nodeDelete);
-
-// Pointer Functions
-NODE* NODE_GetChildWithKey(NODE* self, char* sKey);
-void NODE_IncreaseChildArraySize(NODE* self);
-void NODE_SetInt(NODE* self, ULONG ulValue);
-void NODE_SetString(NODE* self, char* pszText);
-ULONG NODE_GetInt(NODE* self);
-char* NODE_GetString(NODE* self);
-int NODE_GetType(NODE* self);
-void NODE_Print(NODE* self);
-void* NODE_GetValue(NODE* self);
-void NODE_SetValue(NODE* self, int iValueType, void* nodeValue);
+#include "nodehandler.h"
 
 // Global variables
 NODE* rootNode;
@@ -192,6 +128,7 @@ void DeleteByNode(NODE* nodeDelete)
 	destructNode(nodeDelete);
 }
 
+// Takes inputkey "strings.no.*" and calls function pointer with nodekey and value
 void Enumerate(char* nodeKey, void (*Callback)(char* nodeName, void* nodeValue))
 {
 	if(nodeKey == NULL || Callback == NULL) { return; }
---
+++ nodehandler.h
@@ -0,0 +1,67 @@
+#ifndef _NODEHANDLER_H_
+#define _NODEHANDLER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NODE_NAME_BUFFER_SIZE 256
+#define NODE_CHILD_SIZE_INCREMENTER 5
+#define FILEPATH "../Data/data.txt"
+
+// Node types
+#define TYPE_NUMERIC 0
+#define TYPE_STRING 1
+#define TYPE_FOLDER 2
+
+// Node structure
+typedef unsigned long ULONG;
+typedef struct _NODE 
+{
+	char* pszName; 
+	ULONG ulIntVal; 
+	char* pszString;
+	int iNodes; 
+	int iArraySizeChildNodes;
+	struct _NODE** pnNodes;
+	
+	//Function pointers
+	struct _NODE* (*GetChildWithKey)(struct _NODE* self, char* key);
+	void (*IncreaseChildArray)(struct _NODE* self);
+	void (*SetInt)(struct _NODE* self, ULONG ulValue);
+	void (*SetString)(struct _NODE* self, char* pszText);
+	ULONG (*GetInt)(struct _NODE* self);
+	char* (*GetString)(struct _NODE* self);
+	int (*GetType)(struct _NODE* self);
+	void (*Print)(struct _NODE* self);
+	void* (*GetValue)(struct _NODE* self);
+	void (*SetValue)(struct _NODE* self, int iValueType, void* nodeValue);
+	
+} NODE;
+
+// Headers
+FILE* loadFile(char* filePath);
+NODE* loadNodesFromFile(char* filePath);
+int findNodeCountInString(char* pszNodeData);
+int parseNodeData(char* pszNodeData);
+char* findNodeNames(char* pszNodeData, int iNodeCount);
+char* findNodeValue(char* pszNodeData);
+NODE* newNode(char* pszName);
+NODE* findNodeByKey(char* nodeKey);
+void Enumerate(char* nodeKey, void (*Callback)(char* nodeName, void* nodeValue));
+void callbackPrint(char* nodeName, void* nodeValue);
+void Delete(char* nodeKey);
+void DeleteByNode(NODE* nodeDelete);
+
+// Pointer Functions
+NODE* NODE_GetChildWithKey(NODE* self, char* sKey);
+void NODE_IncreaseChildArraySize(NODE* self);
+void NODE_SetInt(NODE* self, ULONG ulValue);
+void NODE_SetString(NODE* self, char* pszText);
+ULONG NODE_GetInt(NODE* self);
+char* NODE_GetString(NODE* self);
+int NODE_GetType(NODE* self);
+void NODE_Print(NODE* self);
+void* NODE_GetValue(NODE* self);
+void NODE_SetValue(NODE* self, int iValueType, void* nodeValue);
+#endif
